78cc514c763e76662b152718b852e6dbba02cfa5
GitHub Action <action@github.com>
2026-04-03 10:50:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/tjlee/secondexecutebutton/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tjlee/secondexecutebutton/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.tjlee.secondexecutebutton.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tjlee.secondexecutebutton.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
